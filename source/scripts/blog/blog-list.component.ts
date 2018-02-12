d7f2e8d13d256cf294c4fc69c1c6f25b9aa4b1f5
Josh Hills <josh.hills@ymail.com>
2018-02-12 21:01:55 +0000

    Added insets

--- source/scripts/blog/blog-list.component.ts
+++ source/scripts/blog/blog-list.component.ts
@@ -1,13 +1,27 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, Input, OnInit, OnChanges, SimpleChanges } from '@angular/core';
 import { BlogPost } from 'shared/models/blog-post';
 
 @Component({
   selector: 'blog-list',
   templateUrl: '../../templates/blog/blog-list.component.html'
 })
-export class BlogListComponent implements OnChanges {
+export class BlogListComponent implements OnChanges, OnInit {
+    @Input() fresh: boolean = true;
     @Input() blogPosts: BlogPost[];
     @Input() excludedPostIds: string[];
+
+    ngOnInit() {
+      this.removeExcludedBlogPosts();
+    }
+
+    removeExcludedBlogPosts(): void {
+      for (let id of this.excludedPostIds) {
+        let index = this.blogPosts.map(p => p.id).indexOf(id);
+        if (index !== -1) {
+          this.blogPosts.splice(index, 1);
+        }
+      }
+    }
 
     ngOnChanges(changes: SimpleChanges) {
       if (changes['blogPosts']) {
@@ -16,5 +30,6 @@ export class BlogListComponent implements OnChanges {
       if (changes['excludedPostIds']) {
         this.excludedPostIds = changes.excludedPostIds.currentValue;
       }
+      this.removeExcludedBlogPosts();
     }
 }
