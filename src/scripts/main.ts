b0292ee0bc21a0ba22fc29c9c037e9d3b3c73585
Josh Hills <josh.hills@ymail.com>
2019-03-02 21:33:31 +0000

    Added console messaging

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -4,9 +4,14 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from '../environments/environment';
 
+import { snooper } from './snooper';
+
 if (environment.production) {
   enableProdMode();
 }
+
+// Say hello to curious eyes...
+console.log('%c%s', snooper.css, snooper.message);
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
---
+++ src/scripts/snooper.ts
@@ -0,0 +1,11 @@
+export const snooper = {
+    message: `Looking for something? You can use the blog to search for specific topics, or contact me directly at josh@jargonify.com
+
+This site was developed with mobile in mind. It uses Angular, and its footprint is < 300kb just scripts/styles compressed, with a service-worker on top to cache things.
+
+It's not perfect, but the source code can be found at:
+https://github.com/joshhills/jargonify-site
+
+~ Josh`,
+    css: 'color: #ffa351;'
+};
